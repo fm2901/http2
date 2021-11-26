f890de16ad1c6ab2cd24535c79932a0e18ec14b7
muminf <fayzievmuminjon@gmail.com>
2021-11-26 18:36:02 +0500

    change HandlerFunc argement type

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -5,138 +5,88 @@ import (
 	"io"
 	"log"
 	"net"
-	"strconv"
 	"strings"
 	"sync"
 )
 
-type HandlerFunc func(conn net.Conn)
+type HandleFunc func(conn net.Conn)
 
 type Server struct {
 	addr     string
 	mu       sync.RWMutex
-	handlers map[string]HandlerFunc
+	handlers map[string]HandleFunc
 }
 
 func NewServer(addr string) *Server {
-	return &Server{addr: addr, handlers: make(map[string]HandlerFunc)}
+	return &Server{addr: addr, handlers: make(map[string]HandleFunc)}
 }
 
-func (s *Server) Register(path string, handler HandlerFunc) {
-	s.mu.RLock()
+func (s *Server) Register(path string, handler HandleFunc) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.handlers[path] = handler
-	s.mu.RUnlock()
 }
-
 func (s *Server) Start() error {
 	listener, err := net.Listen("tcp", s.addr)
 	if err != nil {
-		log.Print(err)
 		return err
 	}
+	defer func() {
+		if cerr := listener.Close(); cerr != nil {
+			if err != nil {
+				err = cerr
+				return
+			}
+			log.Println(err)
+		}
+	}()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Print(err)
+			log.Println(err)
 			continue
 		}
-
-		go s.handle(conn)
-	}
-	return nil
-}
-
-func (s *Server) handle(conn net.Conn) {
-	defer conn.Close()
-
-	s.mu.RLock()
-	buf := make([]byte, 4096)
-	n, err := conn.Read(buf)
-	if err == io.EOF {
-		log.Printf("%s", buf[:n])
-	}
-	if err != nil {
-		log.Print(err)
-	}
-
-	data := buf[:n]
-	requestLineDelim := []byte{'\r', '\n'}
-	requestLineEnd := bytes.Index(data, requestLineDelim)
-	if requestLineEnd == -1 {
-
-	}
-
-	requestLine := string(data[:requestLineEnd])
-	parts := strings.Split(requestLine, " ")
-	if len(parts) != 3 {
-
-	}
-
-	for path, handler := range s.handlers {
-		if parts[1] == path {
-			handler(conn)
+		err = s.handleConnection(conn)
+		if err != nil {
+			log.Println(err)
+			continue
 		}
 	}
-	s.mu.RUnlock()
 }
 
-func (s *Server) PrintResponse(conn net.Conn, response string) (err error) {
+func (s *Server) handleConnection(conn net.Conn) (err error) {
 	defer func() {
 		if cerr := conn.Close(); cerr != nil {
 			if err == nil {
 				err = cerr
 				return
 			}
-			log.Print(err)
+			log.Println(err)
 		}
 	}()
-
-	_, err = conn.Write([]byte(
-		"HTTP/1.1 200 OK\r\n" +
-			"Content-Length: " + strconv.Itoa(len(response)) + "\r\n" +
-			"Content-Type: text/html\r\n" +
-			"Connection: close\r\n" +
-			"\r\n" +
-			response,
-	))
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func parseRequest(conn net.Conn) (path string) {
 	buf := make([]byte, 4096)
 	n, err := conn.Read(buf)
-	if err == io.EOF {
+	if err != io.EOF {
 		log.Printf("%s", buf[:n])
 	}
 	if err != nil {
-		log.Print(err)
+		return err
 	}
+	log.Printf("%s", buf[:n])
 
 	data := buf[:n]
-	requestLineDelim := []byte{'\r', '\n'}
+	requestLineDelim := []byte{'\n', '\r'}
 	requestLineEnd := bytes.Index(data, requestLineDelim)
 	if requestLineEnd == -1 {
 
 	}
-
 	requestLine := string(data[:requestLineEnd])
 	parts := strings.Split(requestLine, " ")
-	if len(parts) != 3 {
-
+	for path, handler := range s.handlers {
+		if parts[1] == path {
+			handler(conn)
+		}
 	}
 
-	method, path, version := parts[0], parts[1], parts[2]
-	if method != "GET" {
-
-	}
-
-	if version != "HTTP/1.1" {
-
-	}
-
-	return path
+	return nil
 }
